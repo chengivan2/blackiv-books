c0b44fde1e3c52c34a8bc3d607cc9e3726103036
Ivan Cheng'ole <chengoleivan@gmail.com>
2022-11-04 09:05:49 +0300

    Prototype header made. Results are displayed

--- blackivbooks/src/App.js
+++ blackivbooks/src/App.js
@@ -1,22 +1,54 @@
 import './App.css';
-import { axios } from "axios";
+import {useState} from 'react';
+import axios from "axios";
 
 function App() {
+
+  const [book, setBook] = useState("java");
+  const [result, setResult] = useState([]);
+  const [APIKey, setAPIKey] = useState("AIzaSyA1yyhNpFfei5ed2AVGGkt4ok-QH5DHgGk");
+
+  function handleChange(event) {
+    const book = event.target.value;
+    setBook(book);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    axios.get("https://www.googleapis.com/books/v1/volumes?q=" + book + "&key=" + APIKey).then(
+      data => {
+        setResult(data.data.items);
+      }
+    );
+  }
+
+
   return (
     <div className="grid justify-center min-w-full">
       <div>
         <h2 className='font-bold'>Start Typing to view books</h2>
       </div>
-      <form onSubmit={handeSubmit}>
+      <form onSubmit={handleSubmit}> {/* Event will fire this method when the form is submitted */}
         <div className="py-5">
-          <input type="text" className='bg-blue-300' onChange={handleChange}/>
+          <input type="text" className='bg-blue-300' onChange={handleChange}/> {/* Event will fire this method when the value changes */}
         </div>
 
         <div>
-          <button type="submit" className="rounded-full bg-red-600 hover:bg-red-300 px-4">Submit</button>
+          <button type="submit" className="rounded-md bg-red-600 hover:bg-red-300 px-4">Submit</button>
         </div>
       </form>
+
+      <div className='grid min-w-full'>
+      {result.map(book => (
+        <a target="_blank" href={book.volumeInfo.previewLink}>
+          <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title}/>
+          <p className='py-2'>{book.volumeInfo.title}</p>
+        </a>
+      ))}
+      </div>
     </div>
+
+    
   );
 }
 
---
+++ blackivbooks/src/headerComponent.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import readbooks from './readbooks.png'
+
+class Header extends React.Component {
+    render() {
+      return (
+        <header className='min-h-screen font-sans bg-red-800'>
+
+            <section className='flex flex-col items-center justify-center md:flex-row md:justify-between py-10'>
+
+                <div className='grid space-evenly leading-loose text-center md:pl-10 md:text-left'>
+
+                    <h1 className='text-6xl py-10 group font-black'>
+                        <span className='group-hover:text-white'>BLACK</span> 
+                        <span className='text-white group-hover:text-black'> IV</span> BOOKS
+                    </h1>
+
+                    <p>
+                        
+                        <figure className="mx-auto max-w-screen-md">
+                            <blockquote>
+                                <q className="text-xl italic font-medium">
+                                    So much books, so little time...
+                                </q>
+                            </blockquote>
+
+                            <figcaption className="flex mt-6 space-x-3">
+                                <div className="flex justify-end divide-x-2 divide-gray-500 dark:divide-gray-700">
+                                    <cite className="pr-3 font-medium text-gray-800">- Frank Zappa</cite>
+                                </div>
+                            </figcaption>
+                        </figure>
+
+                        <br/>
+
+                        <span className='text-lg font-normal tracking-normal'>
+                            Search for your desired books to see how you can get them.
+                        </span>
+
+                        <br/>
+                        
+                        <span className='text-lg font-normal tracking-normal'>
+                            Preview every new book to know what they are about.
+                        </span>
+                    
+                    </p>
+
+                </div>
+
+                <div className='lg:flex justify-center'>
+
+                    <img src={readbooks} />
+
+                </div>
+
+            </section>
+
+        </header>
+      )
+    }
+}
+
+export default Header;
